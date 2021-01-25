17daea4de969205c523a93e4b38afdb519ae64ac
Olivier <olivierchemla@gmail.com>
2021-01-25 14:03:03 +0100

    Updating css, images, and HTML

--- script/index.js
+++ script/index.js
@@ -1,10 +1,16 @@
-function toggleMenu() {
-  const menu = document.querySelector("#header__menu");
-  const links = document.querySelector(".header__links");
+const links = document.querySelector(".header__links");
 
+// Toggle Hamburger Menu
+function toggleMenu() {
   if (links.classList.contains("header__links--visible")) {
     links.classList.remove("header__links--visible");
   } else {
     links.classList.add("header__links--visible");
   }
 }
+// Remove Hamburger Menu when the window is resized
+window.addEventListener("resize", function () {
+  if (window.innerWidth >= 1024) {
+    links.classList.remove("header__links--visible");
+  }
+});
